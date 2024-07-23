99612790655be4613a06ff716195dc8e12db812c
Neema <patelneema93@gmail.com>
2024-07-23 14:12:08 -0400

    create state and operator click event

--- cal/src/App.js
+++ cal/src/App.js
@@ -1,5 +1,11 @@
-
+import { useState } from "react";
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+  const ops =['/' , '*', '+','-','.'];
+  const updateCalc = value =>{
+      setCalc(calc + value);
+  }
   const createDigits = () =>{
     const digits =[];
     for(let i = 1; i < 10; i++){
@@ -13,13 +19,14 @@ function App() {
     <div className="App">
      <div className="calculator">
         <div className="display">
-          <span>(0)</span> 0
+         {result ?  <span>(0)</span> :''}
+         {calc || "0"}
         </div>
         <div className="operators">
-          <button>/</button>
-          <button>*</button>
-          <button>+</button>
-          <button>-</button>
+          <button onClick={() => updateCalc('/')}>/</button>
+          <button onClick={() => updateCalc('*')}>*</button>
+          <button onClick={() => updateCalc('+')}>+</button>
+          <button onClick={() => updateCalc('-')}>-</button>
           <button>DEL</button>
         </div>
         <div className="digits">
