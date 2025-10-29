1fbf5e4e05d09e2a549e657f5cf697bb94771893
Neema <patelneema93@gmail.com>
2025-10-29 11:14:28 -0400

    remove icone file

--- cal/src/App.js
+++ cal/src/App.js
@@ -1,13 +1,9 @@
-
 import Calculator from "./component/Calculator";
 function App() {
- 
   return (
     <div className="App">
-     
      <Calculator />
     </div>
   );
 }
-
 export default App;
